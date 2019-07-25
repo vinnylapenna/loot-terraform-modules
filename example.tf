09ed3fb8873ee4187eb4e2a44187e2cb2b03f846
Vinny LaPenna <vinny.lapenna@slalom.com>
2019-07-24 21:27:11 -0500

    Initial example code

---
+++ example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  //ami           = "ami-b374d5a5"
+
+  instance_type = "t2.micro"
+}
